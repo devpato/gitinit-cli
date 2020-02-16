a9217cbeaf1de99d361966b82ffa3e03dddf79ea
Pato <pevargas2730@gmail.com>
2020-02-16 14:16:40 -0500

    displaying name of cli tool

---
+++ index.js
@@ -0,0 +1,10 @@
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
+const files = require("./lib/files");
+
+clear();
+
+console.log(
+  chalk.yellow(figlet.textSync("Wapi", { horizontalLayout: "full" }))
+);
