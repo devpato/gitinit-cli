a25056e84a8a83059c2b1c09e48bebace09b6253
Pato <pevargas2730@gmail.com>
2020-02-17 13:31:00 -0500

    testing code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
 const files = require("./lib/files");
+const github = require("./lib/github");
 
 clear();
 
@@ -9,7 +10,17 @@ console.log(
   chalk.yellow(figlet.textSync("Wapi", { horizontalLayout: "full" }))
 );
 
-if (files.directoryExists(".git")) {
-  console.log(chalk.red("Already a Git repository!"));
-  process.exit();
-}
+// if (files.directoryExists(".git")) {
+//   console.log(chalk.red("Already a Git repository!"));
+//   process.exit();
+// }
+
+const run = async () => {
+  let token = github.getStoredGithubToken();
+  if (!token) {
+    token = await github.getPersonalAccesToken();
+  }
+  console.log(token);
+};
+
+run();
